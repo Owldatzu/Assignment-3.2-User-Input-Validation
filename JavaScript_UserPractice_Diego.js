d3ea7c414309596cb28ee024ca30bca43ed68024
Diego Morales Villafana <130813807+Owldatzu@users.noreply.github.com>
2024-08-11 22:02:55 -0500

    Add files via upload

---
+++ JavaScript_UserPractice_Diego.js
@@ -0,0 +1,33 @@
+// These are all the inputs the user needs to fullfill
+function checkCreds()
+{
+    var firstName = document.getElementById("fname").value;
+    var lastName = document.getElementById("lname").value;
+    var badgeNum = document.getElementById("badgeID").value;
+    // concatenate
+    var fullName = firstName+ " " + lastName;
+
+    if (fullName.length > 20 || fullName.length< 3)
+    {
+        document.getElementById("loginStatus").innerHTML = "full name too long or too short";
+    }
+
+    else if (badgeNum>999  || badgeNum<100)
+    {
+        document.getElementById("loginStatus").innerHTML = "Badge num < 100 or badge num >999"
+    }
+
+   // else if (badgeNum.length  !=3)
+ //   {
+ //       document.getElementById("loginStatus").innerHTML = "Badge is incorect";
+//    }
+
+//This is where the system recognizes the user's input and let's them continue
+    else 
+    {
+        alert("Access Granted!")
+        alert('Welcome ' + firstName)
+        location.replace("Assignment_2.1_FightGame_Diego.html");
+    }
+        
+}
